d2d086356d3950cb9508ee7d228f967b7b74afea
Aaron Boyd <a.boyd91@yahoo.com>
2022-07-05 22:43:48 -0700

    create routes for users, orders and items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+  root to: 'orders#home'
+  resources :orders do
+    resources :items
+  end
 end
