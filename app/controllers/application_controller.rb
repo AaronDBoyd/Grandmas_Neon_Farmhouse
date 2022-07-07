bad9ba82de84515cd62fe553c68c9da1cd93af1f
Aaron Boyd <a.boyd91@yahoo.com>
2022-07-07 02:52:06 -0700

    create cart_total helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :cart_total
+
+  def cart_total(order)
+    total = 0
+    order.items.each do |item|
+      total += item.cost.to_i
+    end
+    total
+  end
 end
